d51692c0c3ea1210f9c9a4d2df39ed72c5b4d10b
GAMBIT <manishman320@gmail.com>
2024-02-27 21:00:17 +0530

    Added isEmpty() function

--- main.cpp
+++ main.cpp
@@ -1,42 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Stack{
-        int top;
-        public:
-            Stack() {top =-1;}
-            int n = 5;
-            int stack[5];
-        public:
-            void push(int x){
-                if (top >= n-1)
-                {
-                    cout<< "Stack is full" << endl;
-                }
-                top++;
-                stack[top] = x;
-                cout<<"Element inserted in the stack" << endl;
-            }
-        public:
-            void pop(){
-                if (top == -1)
-                {
-                    cout<< "Stack is empty" << endl;
-                }
-                top = top-1;
-                
-            }
-        public:
-            void traverse() {
-                for (int i = top; i >= 0; i--)
-                {
-                    cout<< stack[i] << endl;
-                }
-                
-            }
-    };
+class Stack
+{
+    int top;
 
+public:
+    Stack() { top = -1; }
+    int n = 5;
+    int stack[5];
 
+public:
+    void push(int x)
+    {
+        if (top >= n - 1)
+        {
+            cout << "Stack is full" << endl;
+        }
+        top++;
+        stack[top] = x;
+        cout << "Element inserted in the stack" << endl;
+    }
+
+public:
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "Stack is empty" << endl;
+        }
+        top = top - 1;
+    }
+
+public:
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+public:
+    void traverse()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty" << endl;
+        }
+        for (int i = top; i >= 0; i--)
+        {
+            cout << stack[i] << endl;
+        }
+    }
+};
 
 int main()
 {
@@ -47,7 +68,7 @@ int main()
     s.push(8);
     s.traverse();
     s.pop();
-    cout<<endl;
+    cout << endl;
     s.traverse();
     return 0;
 }
