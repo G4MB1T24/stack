1ef6e09cf8e7ee8a6644a0ca2e1992321d094f31
GAMBIT <manishman320@gmail.com>
2024-02-27 20:54:23 +0530

    Array implementation of stack

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+        int top;
+        public:
+            Stack() {top =-1;}
+            int n = 5;
+            int stack[5];
+        public:
+            void push(int x){
+                if (top >= n-1)
+                {
+                    cout<< "Stack is full" << endl;
+                }
+                top++;
+                stack[top] = x;
+                cout<<"Element inserted in the stack" << endl;
+            }
+        public:
+            void pop(){
+                if (top == -1)
+                {
+                    cout<< "Stack is empty" << endl;
+                }
+                top = top-1;
+                
+            }
+        public:
+            void traverse() {
+                for (int i = top; i >= 0; i--)
+                {
+                    cout<< stack[i] << endl;
+                }
+                
+            }
+    };
+
+
+
+int main()
+{
+    Stack s;
+    s.push(6);
+    s.push(5);
+    s.push(7);
+    s.push(8);
+    s.traverse();
+    s.pop();
+    cout<<endl;
+    s.traverse();
+    return 0;
+}
